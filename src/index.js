3203a3927f39c62f08182e82068ab7d5ea3142b0
Sergey <sergeyproshko2005@gmail.com>
2024-08-10 18:25:32 +0300

    feat: task 19/20 complited!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('')
+    if ((arr[arr.length - 1] == ')' && arr[arr.length - 3] == '(') || (arr[arr.length - 1] == ')' && arr[arr.length - 2] == '|')){
+      return false
+    }
+
+    if (arr[1] == '[' && arr[arr.length - 2] == '('){
+      return false
+    }
+
+    if (arr[arr.length - 1] == ')' || arr[arr.length - 1] == '|' || arr[arr.length - 1] == '2'){
+      return true
+    }
+    return false
 }
