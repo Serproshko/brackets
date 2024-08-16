31c34000327b82a0f4f6599b8106eb434c08a872
Sergey <sergeyproshko2005@gmail.com>
2024-08-16 13:29:46 +0300

    feat: task complited!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
   let arr = str.split('')
+    if ((arr[arr.length - 1] == ')' && arr[arr.length - 4] == '(')){
+      return true
+    }
+
     if ((arr[arr.length - 1] == ')' && arr[arr.length - 3] == '(') || (arr[arr.length - 1] == ')' && arr[arr.length - 2] == '|')){
       return false
     }
-
+    
     if (arr[1] == '[' && arr[arr.length - 2] == '('){
       return false
     }
